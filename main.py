4b235cc2f0c8f7109370a689f44d5a2f5e4d0941
WolfgangZimm26 <100966871+WolfgangZimm26@users.noreply.github.com>
2023-12-19 13:08:56 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import my_module  # Import your module
+
+app = FastAPI()
+
+class InputData(BaseModel):
+    prompt: str
+
+@app.post("/process")
+def process_input(data: InputData):
+    try:
+        result = my_module.process_prompt(data.prompt)
+        return result
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
---
+++ my_module.py
@@ -0,0 +1,99 @@
+import openai
+import torch
+from transformers import BertTokenizer, BertModel
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+from scipy import stats
+import json
+from itertools import combinations
+import time
+import random
+
+
+def get_openai_responses(prompt, num_responses=40, max_tokens=100, initial_delay: float = 1,
+                         exponential_base: float = 2, jitter: bool = True):
+    delay = initial_delay
+    responses = []
+    retries = 0
+    max_retries = 10
+
+    while len(responses) < num_responses and retries < max_retries:
+        try:
+            AI_response = openai.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "You are a helpful assistant that provides meal recommendations."},
+                    {"role": "user", "content": prompt},
+                ],
+                max_tokens=max_tokens,
+                n=1,  # Number of completions to generate
+            )
+            response_content = AI_response["choices"][0]["message"]["content"]
+            responses.append(response_content)
+        except openai.RateLimitError:
+            delay *= exponential_base * (1 + jitter * random.random())  # implementation of exponential backing off
+            print(f"Rate limit exceeded. Waiting for {delay} seconds.")
+            time.sleep(delay)
+            retries += 1
+
+    if retries == max_retries:
+        print("Max retries reached. Unable to get a successful response.")
+
+    return responses
+
+
+def get_bert_embedding(my_text):
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    model = BertModel.from_pretrained('bert-base-uncased')
+    tokens = tokenizer(my_text, return_tensors='pt')
+    with torch.no_grad():
+        outputs = model(**tokens)
+    last_hidden_states = outputs.last_hidden_state
+    embedding = torch.mean(last_hidden_states, dim=1)
+    return embedding.numpy().tolist()
+
+
+def process_prompt(prompt):
+    all_quant_scores = []  # Store quant scores for all prompts
+    responses = get_openai_responses(prompt)
+
+    prompt_embedding = get_bert_embedding(prompt)
+    response_embeddings = [get_bert_embedding(response) for response in responses]
+
+    cosine_sim_scores = []
+    embedding_pairs = list(combinations([prompt_embedding] + response_embeddings, 2))
+    for pair in embedding_pairs:
+        score = cosine_similarity([pair[0]], [pair[1]])[0][0]
+        cosine_sim_scores.append(score)
+
+    mean_score = np.mean(cosine_sim_scores)
+    median_score = np.median(cosine_sim_scores)
+    mode_result = stats.mode(cosine_sim_scores)
+    mode_score = float(mode_result.mode[0])
+    average_score = np.average(cosine_sim_scores)
+    sample_size = len(cosine_sim_scores)
+
+    quant_scores = {
+        "mean": mean_score,
+        "median": median_score,
+        "mode": mode_score,
+        "average": average_score,
+        "sample_size": sample_size
+    }
+
+    all_quant_scores.append({"prompt": prompt, "quant_scores": quant_scores})
+
+    result_data = {
+        "all_quant_scores": all_quant_scores
+    }
+
+    return result_data
+
+
+if __name__ == "__main__":
+    # Example of how to use the module
+    prompt_example = "What is the best meal for dinner?"
+    result = process_prompt(prompt_example)
+
+    with open('results.json', 'w') as f:
+        json.dump(result, f, indent=2)
