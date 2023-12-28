cb3b12ad4cad08a934cf1671d350ab6b6dd7d3f5
Garrett Zimmerman <gzimmerman26@yahoo.com>
2023-12-28 11:35:03 -0700

    Fixed Functions

--- API/my_module.py
+++ API/my_module.py
@@ -21,6 +21,24 @@ client = OpenAI(
 
 def get_openai_responses(prompt, num_responses=40, max_tokens=100, initial_delay: float = 1,
                          exponential_base: float = 2, jitter: bool = True):
+    
+    """
+    Retrieves a specified number of responses from OpenAI's GPT model.
+    
+    Args:
+        prompt (str): The prompt to send to the model.
+        num_responses (int): The number of responses to retrieve.
+        max_tokens (int): The maximum number of tokens per response.
+        initial_delay (float): Initial delay in seconds before retrying after a rate limit error.
+        exponential_base (float): The base for the exponential backoff calculation.
+        jitter (bool): Whether to add random jitter to the delay.
+
+    Returns:
+        list: A list of responses from the AI model.
+    """
+    if num_responses > 100:  # Example threshold
+        raise ValueError("num_responses is too high. Please reduce the number.")
+
     delay = initial_delay
     responses = []
     retries = 0
@@ -37,17 +55,23 @@ def get_openai_responses(prompt, num_responses=40, max_tokens=100, initial_delay
                 max_tokens=max_tokens,
                 n=1,  # Number of completions to generate
             )
-            response_content = AI_response["choices"][0]["message"]["content"]
+            #print(AI_response)
+            #need to eventually un comment these
+            response_content = AI_response.choices[0].message.content
             responses.append(response_content)
-        except  Exception as e:
+        except Exception as e:
+            print(f"Encountered an error: {e}")
             if 'rate limit' in str(e).lower():
-                delay *= exponential_base * (1 + jitter * random.random())  # implementation of exponential backing off
+                delay *= exponential_base * (1 + jitter * random.random())
                 print(f"Rate limit exceeded. Waiting for {delay} seconds.")
                 time.sleep(delay)
-                retries += 1
+            else:
+                print("Encountered a non-rate-limit error. Retrying...")
+            retries += 1
+            time.sleep(initial_delay)  # Basic delay for non-rate-limit errors
 
     if retries == max_retries:
-        print("Max retries reached. Unable to get a successful response.")
+        print("Max retries reached. Some responses may not have been retrieved.")
 
     return responses
 
@@ -60,37 +84,51 @@ def get_bert_embedding(my_text):
         outputs = model(**tokens)
     last_hidden_states = outputs.last_hidden_state
     embedding = torch.mean(last_hidden_states, dim=1)
-    return embedding.numpy().tolist()
+    
+    # Convert to numpy array and check shape
+    embedding_np = embedding.numpy()
+    #print(f"Embedding shape: {embedding_np.shape}")  # For debugging
+    return embedding_np.tolist()
 
 
 def process_prompt(prompt):
     result_data = []  # Store quant scores for all prompts
     responses = get_openai_responses(prompt)
 
-    prompt_embedding = get_bert_embedding(prompt)
+    prompt_embedding = get_bert_embedding(prompt)  # This returns a list of lists
     response_embeddings = [get_bert_embedding(response) for response in responses]
 
     cosine_sim_scores = []
+    # Create combinations of the prompt embedding and all response embeddings
     embedding_pairs = list(combinations([prompt_embedding] + response_embeddings, 2))
     for pair in embedding_pairs:
-        score = cosine_similarity([pair[0]], [pair[1]])[0][0]
+        # Since the embeddings are lists, they can be directly used
+        score = cosine_similarity([pair[0][0]], [pair[1][0]])[0][0]
         cosine_sim_scores.append(score)
 
-    mean_score = np.mean(cosine_sim_scores)
-    median_score = np.median(cosine_sim_scores)
-    mode_score = stats.mode(cosine_sim_scores)
-    average_score = np.average(cosine_sim_scores)
-    sample_size = len(cosine_sim_scores)
+        # Calculate statistical measures
+        mean_score = np.mean(cosine_sim_scores)
+        median_score = np.median(cosine_sim_scores)
+        mode_result = stats.mode(cosine_sim_scores, keepdims=False)
 
-    quant_scores = {
-        "mean": mean_score,
-        "median": median_score,
-        "mode": mode_score,
-        "average": average_score,
-        "sample_size": sample_size
-    }
 
-    result_data.append({"prompt": prompt, "quant_scores": quant_scores})
+        # Check if the mode result is a scalar or an array and extract the mode value
+        if np.isscalar(mode_result.mode):
+            mode_score = mode_result.mode
+        else:
+            mode_score = mode_result.mode[0] if mode_result.mode.size else None
+        average_score = np.average(cosine_sim_scores)
+        sample_size = len(cosine_sim_scores)
+
+        quant_scores = {
+            "mean": mean_score,
+            "median": median_score,
+            "mode": mode_score,
+            "average": average_score,
+            "sample_size": sample_size
+        }
+
+        result_data.append({"prompt": prompt, "quant_scores": quant_scores})
 
     return result_data
 
